d099786538db09826b20004ea622ed2323641dc8
harsh-borse <harshborse202372@gmail.com>
2026-08-21 16:19:56 +0530

    Age

--- BankAccNo.py
+++ BankAccNo.py
@@ -5,9 +5,11 @@
 #if the Balance is >= 100000 then interest is 5%
 #else no Interest is given.
 #Display AccNo, Balance and Interest Amount.
+#In case of a senior citizen (age >= 70), give additional 1% interest on Balance + Interest.
 
 acc_no = int(input("Enter Bank Account Number: "))
 balance = float(input("Enter Balance: "))
+age = int(input("Enter Age: "))
 
 if balance >= 1000000:
     rate = 10
@@ -19,6 +21,9 @@ else:
     rate = 0
 
 interest = (balance * rate) / 100
+if age>=70:
+    add_interest = (balance + interest) * 1 / 100
+    interest = interest + add_interest
 
 print("Account Number:", acc_no)
 print("Balance:", balance)
