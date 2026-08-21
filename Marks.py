9fb27091d74616f69b10ec2cc34ca7f3c769b67d
harsh-borse <harshborse202372@gmail.com>
2026-08-21 16:26:33 +0530

    Marks

---
+++ Marks.py
@@ -0,0 +1,26 @@
+#Write a program to accept marks of 3 Subjects out of 100.
+#Calculate total marks and percentage and Calculate Grade as following
+#if percentage is >=90, grade is "Distinction"
+#if percentage is >=80, grade is "First Class"
+#if percentage is >=60, grade is "Second Class"
+#if percentage is <35, grade is "Fail"
+
+marks1 = int(input("Enter marks for subject 1: "))
+marks2 = int(input("Enter marks for subject 2: "))
+marks3 = int(input("Enter marks for subject 3: "))
+
+total_marks = marks1 + marks2 + marks3
+percentage = (total_marks / 300) * 100
+
+if percentage >= 90:
+    grade = "Distinction"
+elif percentage >= 80:
+    grade = "First Class"
+elif percentage >= 60:
+    grade = "Second Class"
+else:
+    grade = "Fail"
+
+print("Total Marks:", total_marks)
+print("Percentage:", percentage)
+print("Grade:", grade)
